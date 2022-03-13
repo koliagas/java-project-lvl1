672e822f8cbe28abb0ce710da7b889ba0319c775
Nick <koli.1997@gmail.com>
2022-03-13 19:15:58 +0300

    Add greeting with user in App.java and standart input in build.gradle to use ./gradlew run

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void getName() {
+        System.out.println("May I have your name?");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
