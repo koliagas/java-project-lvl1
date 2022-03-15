0ee66d7308b5d05bcb6ddc103fef94408247ec10
Nick <koli.1997@gmail.com>
2022-03-15 18:04:21 +0300

    Update Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Cli {
     public static String getName() {
-        System.out.println("May I have your name?");
+        System.out.print("May I have your name? ");
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
         System.out.println("Hello, " + name + "!");
