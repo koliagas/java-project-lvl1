56e9e11ae6f849c7441bfdfc200ab61f6f30821f
Nick <koli.1997@gmail.com>
2022-03-16 17:20:24 +0300

    Add new game in App and Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -14,35 +14,55 @@ public class Engine {
     }
     public static void engine(int choiceGame) {
         final int maxRound = 3;
+        final int greet = 1;
         final int parityGame = 2;
         final int calcGame = 3;
         final int gcdGame = 4; // greatest common divisor
         final int progressionGame = 5;
-        System.out.println("Welcome to the Brain Games!");
-        String userName = Cli.getName();
-        final int randUpperLimit = 10;
+        final int primeGame = 6;
+        final int randUpperLimit = 100;
+        String userName;
+
         switch (choiceGame) {
+            case greet:
+                System.out.println("Welcome to the Brain Games!");
+                userName = Cli.getName();
             case parityGame:
+                System.out.println("Welcome to the Brain Games!");
+                userName = Cli.getName();
                 for (int i = 0; i < maxRound; i++) {
                     boolean checkGamePass = Parity.parityGame(randUpperLimit);
                     check(checkGamePass, i, userName);
                 }
                 break;
             case calcGame:
+                System.out.println("Welcome to the Brain Games!");
+                userName = Cli.getName();
                 for (int i = 0; i < maxRound; i++) {
                     boolean checkGamePass = Calc.calcGame(i, randUpperLimit);
                     check(checkGamePass, i, userName);
                 }
                 break;
             case gcdGame:
+                System.out.println("Welcome to the Brain Games!");
+                userName = Cli.getName();
                 for (int i = 0; i < maxRound; i++) {
                     boolean checkGamePass = Gcd.gcdGame(randUpperLimit);
                     check(checkGamePass, i, userName);
                 }
                 break;
             case progressionGame:
+                System.out.println("Welcome to the Brain Games!");
+                userName = Cli.getName();
                 for (int i = 0; i < maxRound; i++) {
                     boolean checkGamePass = ProgressionGame.progressGame(randUpperLimit);
+                    check(checkGamePass, i, userName);
+                }
+            case primeGame:
+                System.out.println("Welcome to the Brain Games!");
+                userName = Cli.getName();
+                for (int i = 0; i < maxRound; i++) {
+                    boolean checkGamePass = PrimeGame.primeGame(randUpperLimit);
                     check(checkGamePass, i, userName);
                 }
                 break;
